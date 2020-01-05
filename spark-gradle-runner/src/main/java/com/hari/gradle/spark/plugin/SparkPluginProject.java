07c6bcfb2aba1c6d5b8fb6804ed0b650200ca4b3
krishari2020 <krishari.erode2020@gmail.com>
2020-01-05 17:36:07 +0530

    Code refactoring , some functional abstractions required to make reusable code.

--- spark-gradle-runner/src/main/java/com/hari/gradle/spark/plugin/SparkPluginProject.java
+++ spark-gradle-runner/src/main/java/com/hari/gradle/spark/plugin/SparkPluginProject.java
@@ -2,9 +2,11 @@ package com.hari.gradle.spark.plugin;
 
 import static com.hari.gradle.spark.plugin.Settings.SETTINGS_EXTN;
 import static com.hari.gradle.spark.plugin.SparkRunMode.getRunMode;
+import static java.util.Arrays.asList;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.function.Function;
 
 import org.gradle.api.Action;
 import org.gradle.api.Plugin;
@@ -37,30 +39,26 @@ public class SparkPluginProject implements Plugin<Project> {
 	@Override
 	public void apply(Project p) {
 		p.getPluginManager().apply(org.gradle.api.plugins.scala.ScalaPlugin.class);
-		p.getRepositories().add(p.getRepositories().mavenCentral());
-		p.getRepositories().add(p.getRepositories().mavenLocal());
-		p.getRepositories().add(p.getRepositories().maven(new Action<MavenArtifactRepository>() {
-
-			@Override
-			public void execute(MavenArtifactRepository customRepo) {
-				try {
-					customRepo.setUrl(new URI("http://repo.gradle.org/gradle/libs-releases-local"));
-				} catch (URISyntaxException urise) {
-					SPGLogger.logError.accept("Incorrect URL provided");
-				}
-			}
-
-		}));
+		p.getRepositories().addAll(asList(p.getRepositories().mavenCentral(), p.getRepositories().mavenLocal(),
+				p.getRepositories().maven(new Action<MavenArtifactRepository>() {
+					@Override
+					public void execute(MavenArtifactRepository customRepo) {
+						try {
+							customRepo.setUrl(new URI("http://repo.gradle.org/gradle/libs-releases-local"));
+						} catch (URISyntaxException urise) {
+							SPGLogger.logError.accept("Incorrect URL provided");
+						}
+					}
+				})));
 		final Settings settings = p.getExtensions().create(SETTINGS_EXTN, Settings.class);
 		Task copyDeps = p.getTasks().create("copyDeps", CopyDepsTask.class);
-		copyDeps.setDescription("Copies all dependencies required to run spark job");
-		copyDeps.setGroup(GROUP);
-		copyDeps.dependsOn("jar");
+		copyDeps = taskInit.apply(copyDeps).apply("Copies all dependencies required to run spark job")
+				.apply(p.task("jar")).apply(GROUP);
+
 		Task prepClusterSubmit = p.getTasks().create("prepareClusterSubmit", PrepareForClusterSubmit.class);
-		prepClusterSubmit
-				.setDescription(" Copies all spark deps into the cluster to create a distributed cache in Yarn");
-		prepClusterSubmit.dependsOn(copyDeps);
-		prepClusterSubmit.setGroup(GROUP);
+		prepClusterSubmit = taskInit.apply(prepClusterSubmit)
+				.apply(" Copies all spark deps into the cluster to create a distributed cache in Yarn").apply(copyDeps)
+				.apply(GROUP);
 		prepClusterSubmit.onlyIf(new Spec<Task>() {
 			@Override
 			public boolean isSatisfiedBy(Task arg0) {
@@ -71,9 +69,19 @@ public class SparkPluginProject implements Plugin<Project> {
 			}
 		});
 		Task launch = p.getTasks().create("launch", LaunchSparkTask.class);
-		launch.setDescription("Launch a spark-job with overrided settings");
-		launch.setGroup(GROUP);
-		launch.dependsOn(prepClusterSubmit);
+		launch = taskInit.apply(launch).apply("Launch a spark-job with overrided settings").apply(prepClusterSubmit)
+				.apply(GROUP);
 	}
+
+	private static interface GradleTaskInitializer
+			extends Function<Task, Function<String, Function<Task, Function<String, Task>>>> {
+	}
+
+	private GradleTaskInitializer taskInit = task -> description -> depTask -> taskGroup -> {
+		task.setDescription(description);
+		task.dependsOn(depTask);
+		task.setGroup(taskGroup);
+		return task;
+	};
 
 }
