436166de84635cd17f1aaf6dd81cf4bb7feac5e6
krishari2020 <krishari.erode2020@gmail.com>
2020-01-25 07:40:03 +0530

    Consuming spark-gradle-plugin latest version

--- spark-gradle-runner-tests/src/main/scala/com/hari/gradle/spark/plugin/test/cluster/SrcToTgt.scala
+++ spark-gradle-runner-tests/src/main/scala/com/hari/gradle/spark/plugin/test/cluster/SrcToTgt.scala
@@ -1,6 +1,8 @@
 package com.hari.gradle.spark.plugin.test.cluster
 
 import org.apache.spark.sql.{ SparkSession, SaveMode }
+import org.apache.spark.scheduler.SparkListenerTaskEnd
+import org.apache.spark.scheduler.SparkListener
 
 /**
  * Move data from source to target in cluster mode.
@@ -13,5 +15,31 @@ object SrcToTgt {
   def main(arsg: Array[String]): Unit = {
     val spark = SparkSession.builder.getOrCreate()
     spark.read.json("/hari/source.json").write.mode(SaveMode.Overwrite).json("/hari/target.json")
+    spark.sparkContext.addSparkListener(new SparkListener() {
+      override def onTaskEnd(taskEnd: SparkListenerTaskEnd): Unit = {
+
+        // The following section contains respective "metrics" object values.
+        val inputMetrics = taskEnd.taskMetrics.inputMetrics
+        val outputMetrics = taskEnd.taskMetrics.outputMetrics
+        val shuffleReadMetrics = taskEnd.taskMetrics.shuffleReadMetrics
+        val shuffleWriteMetrics = taskEnd.taskMetrics.shuffleWriteMetrics
+
+        // The following section contains "metrics" primitive values
+        val diskSpilled = taskEnd.taskMetrics.diskBytesSpilled
+        val execCpuTime = taskEnd.taskMetrics.executorCpuTime
+        val execDeCpuTime = taskEnd.taskMetrics.executorDeserializeCpuTime
+        val execDeTime = taskEnd.taskMetrics.executorDeserializeTime
+        val execRunTime = taskEnd.taskMetrics.executorRunTime
+        val jvmGCTime = taskEnd.taskMetrics.jvmGCTime
+        val memBytesSpilled = taskEnd.taskMetrics.memoryBytesSpilled
+        val peakExecMem = taskEnd.taskMetrics.peakExecutionMemory
+        val resSerTime = taskEnd.taskMetrics.resultSerializationTime
+        val resultSize = taskEnd.taskMetrics.resultSize
+
+        // Following section contains metrics for block related information.
+        val blockInfo = taskEnd.taskMetrics.updatedBlockStatuses
+      }
+    })
   }
+
 }
