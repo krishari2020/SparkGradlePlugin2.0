965053d8544bf6e0522d0034373bd297f42fa2c4
krishari2020 <krishari.erode2020@gmail.com>
2022-02-17 23:00:27 +0530

    Reverting changes done to LaunchMainSpark and LaunchSparkTask as a separate task for launching spark in k8s is being implemented

--- spark-gradle-runner/src/main/java/com/hari/gradle/spark/plugin/tasks/LaunchMainSpark.java
+++ spark-gradle-runner/src/main/java/com/hari/gradle/spark/plugin/tasks/LaunchMainSpark.java
@@ -1,156 +1,178 @@
 package com.hari.gradle.spark.plugin.tasks;
 
-import com.hari.gradle.spark.plugin.SPGLogger;
-import org.apache.spark.launcher.SparkLauncher;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
-import static com.hari.gradle.spark.plugin.Constants.*;
+import static com.hari.gradle.spark.plugin.Constants.DISTRIBUTED_YARN_CACHE_PATH;
+import static com.hari.gradle.spark.plugin.Constants.HADOOP_HOME;
+import static com.hari.gradle.spark.plugin.Constants.HADOOP_USER_NAME;
+import static com.hari.gradle.spark.plugin.Constants.SPARK_CONF_DEPLOY_MODE;
+import static com.hari.gradle.spark.plugin.Constants.SPARK_CONF_YARN_ZIP;
+import static com.hari.gradle.spark.plugin.Constants.SPARK_MAIN_CLASSPATH;
+import static com.hari.gradle.spark.plugin.Constants.YARN_CONF_DIR;
 import static com.hari.gradle.spark.plugin.SPGLogger.PROPERTY_SET_VALUE;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import org.apache.spark.launcher.SparkLauncher;
+
+import com.hari.gradle.spark.plugin.SPGLogger;
+
 /**
  * The main class which would execute the SparkJob via SparkLauncher.
- *
+ * 
  * @author harim
+ *
  */
 
 public class LaunchMainSpark {
 
-    public static final Function<String, List<String>> parseConf = conf -> asList(conf.split("\\s*,\\s*spark.")).stream().filter(con -> con != null).map(con1 -> con1.trim()).filter(con2 -> !con2.isEmpty()).map(parsed -> "spark." + parsed).collect(toList());
-    public static final Function<String, List<String>> parseConf2 = conf -> asList(conf.split(";")).stream().filter(Objects::isNull).filter(conf2 -> conf2.contains("=")).collect(toList());
-    /**
-     * @param args[0]
-     * - Name of the spark app to be executed.
-     * @param args[1]
-     * - Master (can be local , cluster)
-     * @param args[2]
-     * - Jar containing the spark application to be run , added as
-     * appResource.
-     * @param args[3]
-     * - The class which contains the spark job representing the
-     * application.
-     * @param args[4]
-     * - classPath for driver and executor.
-     * @param args[5]
-     * - File to which standard error will be re-directed to.
-     * @param args[6]
-     * - File to which standard out will be re-directed to.
-     * @param args[7]
-     * - Spark Home location.
-     * @param args[8]
-     * - Spark user overridden configs properties file path.
-     */
+	/**
+	 * @param args[0]
+	 *            - Name of the spark app to be executed.
+	 * @param args[1]
+	 *            - Master (can be local , cluster)
+	 * @param args[2]
+	 *            - Jar containing the spark application to be run , added as
+	 *            appResource.
+	 * @param args[3]
+	 *            - The class which contains the spark job representing the
+	 *            application.
+	 * @param args[4]
+	 *            - classPath for driver and executor.
+	 * @param args[5]
+	 *            - File to which standard error will be re-directed to.
+	 * @param args[6]
+	 *            - File to which standard out will be re-directed to.
+	 * @param args[7]
+	 *            - Spark Home location.
+	 * @param args[8]
+	 *            - Spark user overridden configs.
+	 */
 
-    private static final Predicate<String> isValidConf = conf -> conf != null && !conf.isEmpty() && !conf.equals("EMPTY");
+	private static final Predicate<String> isValidConf = conf -> conf != null && !conf.isEmpty()
+			&& !conf.equals("EMPTY");
 
-    public static void main(String args[]) {
-        if (args == null || args.length != 9)
-            throw new IllegalArgumentException(String.format("Number of program args violated , expected 9 args but actual is %s", args.length));
-        String appName = args[0];
-        String master = args[1];
-        String jarPath = args[2];
-        String mainClass = args[3];
-        String errFile = args[4];
-        String outFile = args[5];
-        String sparkHome = args[6];
-        String sparkConfFile = args[7];
-        String dockerImageId = args[8];
+	public static final Function<String, List<String>> parseConf = conf -> asList(conf.split("\\s*,\\s*spark."))
+			.stream().filter(con -> con != null).map(con1 -> con1.trim()).filter(con2 -> !con2.isEmpty())
+			.map(parsed -> "spark." + parsed).collect(toList());
 
-        // look for if env vars YARN_CONF_DIR and HADOOP_HOME have been set
-        // if so it would be needed to submit application to yarn.
-        Map<String, String> envs = Environment.toMap(Environment.newInstance(YARN_CONF_DIR, System.getenv(YARN_CONF_DIR)), Environment.newInstance(HADOOP_HOME, System.getenv(HADOOP_HOME)), Environment.newInstance(HADOOP_USER_NAME, System.getenv(HADOOP_USER_NAME)));
-        String yarnDistributedClassPath = System.getenv(DISTRIBUTED_YARN_CACHE_PATH);
-        String classPath = System.getenv(SPARK_MAIN_CLASSPATH);
-        String yarnClusterMode = System.getenv(SPARK_CONF_DEPLOY_MODE);
-        Optional<SparkLauncher> launcher = Optional.ofNullable(!envs.isEmpty() && envs.size() == 3 ? new SparkLauncher(envs) : new SparkLauncher());
-        try {
-            Process launch = launcher.map(launch1 -> launch1.setAppName(appName).setMaster(master).setMainClass(mainClass).setSparkHome(sparkHome).addJar(jarPath).setAppResource(jarPath).redirectError(new File(errFile)).redirectOutput(new File(outFile)).setVerbose(true)).map(launch2 -> {
-                if (yarnDistributedClassPath != null && !yarnDistributedClassPath.isEmpty()) {
-                    SPGLogger.logFine.accept("Launching the application in yarn mode and that requires uploading the deps to distributed yarn cache");
-                    SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(SPARK_CONF_YARN_ZIP, yarnDistributedClassPath));
-                    return launch2.setConf(SPARK_CONF_YARN_ZIP, yarnDistributedClassPath).setDeployMode(yarnClusterMode);
-                } else if (master.startsWith("k8s")) {
-                    // Run on k8s cluster
-                    if (dockerImageId == null || dockerImageId.isBlank())
-                        throw new IllegalArgumentException("Docker image id cannot be empty or null when running" +
-                                "in k8s");
-                    launch2.setConf(SPARK_K8S_CONTAINER_IMAGE, dockerImageId);
-                    return launch2;
-                } else {
-                    // In stand-alone mode be sure to set the Driver and Executor classPath.
-                    launch2.setConf(SparkLauncher.DRIVER_EXTRA_CLASSPATH, classPath).setConf(SparkLauncher.EXECUTOR_EXTRA_CLASSPATH, classPath);
-                    return launch2;
-                }
-            }).map(launch3 -> {
-                // if spark conf properties is provided then set
-                // any error handling that is required needs to be handled by Spark Api
-                if (sparkConfFile != null && !sparkConfFile.isBlank())
-                    launch3.setPropertiesFile(sparkConfFile);
-                return launch3;
-            }).get().launch();
-            launch.waitFor();
-            int exit = launch.exitValue();
-            if (exit == 0)
-                SPGLogger.logInfo.accept("Spark job completed successfully ");
-            else if (exit == 1) {
-                SPGLogger.logError.accept("Spark job terminated with errors , please check stdErr.txt and stdOut.txt ");
-                throw new RuntimeException("Spark application failed");
-            }
-        } catch (IOException | InterruptedException e) {
-            SPGLogger.logError.accept("LaunchMainSpark exited with an error " + e.getLocalizedMessage());
-        }
-    }
+	public static final Function<String, List<String>> parseConf2 = conf -> asList(conf.split(";")).stream()
+			.filter(Objects::isNull).filter(conf2 -> conf2.contains("=")).collect(toList());
 
-    /***
-     * Function to parse spark config into corresponding key value confs.
-     *
-     * @param conf
-     *            - unparsed spark configuration as String.
-     * @return Map of key value pairs.
-     */
+	public static void main(String args[]) {
+		if (args == null || args.length != 8)
+			throw new IllegalArgumentException(
+					String.format("Number of program args violated , expected 8 args but actual is %s", args.length));
+		String appName = args[0];
+		String master = args[1];
+		String jarPath = args[2];
+		String mainClass = args[3];
+		String errFile = args[4];
+		String outFile = args[5];
+		String sparkHome = args[6];
+		String sparkConfig = args[7];
 
-    public static Map<String, String> buildSparkConf(String conf) {
-        final List<String> parsedConf = Optional.ofNullable(conf).filter(isValidConf).flatMap(conf1 -> Optional.of(parseConf2.apply(conf1))).orElse(Collections.emptyList());
-        return Environment.toMap(parsedConf.stream().map(cnf -> {
-            int eqIndex = cnf.indexOf("=");
-            String key = cnf.substring(0, eqIndex);
-            String value = cnf.substring(eqIndex + 1);
-            if (key == null) {
-                SPGLogger.logError.accept("Spark conf key cannot be null");
-                throw new IllegalArgumentException("Incorrect spark config values, check the log for more details.");
-            }
-            return Environment.newInstance(key, value);
-        }).collect(toList()));
-    }
+		// look for if env vars YARN_CONF_DIR and HADOOP_HOME have been set
+		// if so it would be needed to submit application to yarn.
+		Map<String, String> envs = Environment.toMap(
+				Environment.newInstance(YARN_CONF_DIR, System.getenv(YARN_CONF_DIR)),
+				Environment.newInstance(HADOOP_HOME, System.getenv(HADOOP_HOME)),
+				Environment.newInstance(HADOOP_USER_NAME, System.getenv(HADOOP_USER_NAME)));
+		String yarnDistributedClassPath = System.getenv(DISTRIBUTED_YARN_CACHE_PATH);
+		String classPath = System.getenv(SPARK_MAIN_CLASSPATH);
+		String yarnClusterMode = System.getenv(SPARK_CONF_DEPLOY_MODE);
+		Optional<SparkLauncher> launcher = Optional
+				.ofNullable(!envs.isEmpty() && envs.size() == 3 ? new SparkLauncher(envs) : new SparkLauncher());
+		try {
+			Process launch = launcher.map(launch1 -> launch1.setAppName(appName).setMaster(master)
+					.setMainClass(mainClass).setSparkHome(sparkHome).addJar(jarPath).setAppResource(jarPath)
+					.redirectError(new File(errFile)).redirectOutput(new File(outFile)).setVerbose(true))
+					.map(launch2 -> {
+						if (yarnDistributedClassPath != null && !yarnDistributedClassPath.isEmpty()) {
+							SPGLogger.logFine.accept(
+									"Launching the application in yarn mode and that requires uploading the deps to distributed yarn cache");
+							SPGLogger.logFine
+									.accept(PROPERTY_SET_VALUE.apply(SPARK_CONF_YARN_ZIP, yarnDistributedClassPath));
+							return launch2.setConf(SPARK_CONF_YARN_ZIP, yarnDistributedClassPath)
+									.setDeployMode(yarnClusterMode);
+						} else {
+							// In stand-alone mode be sure to set the Driver and Executor classPath.
+							return launch2.setConf(SparkLauncher.DRIVER_EXTRA_CLASSPATH, classPath)
+									.setConf(SparkLauncher.EXECUTOR_EXTRA_CLASSPATH, classPath);
+						}
+					}).map(launch3 -> {
+						// parse out the comma separated spark configs
+						// each parsed record contains key separated by "=" followed by its value.
+						buildSparkConf(sparkConfig).forEach((key, value) -> launch3.setConf(key, value));
+						return launch3;
+					}).get().launch();
+			launch.waitFor();
+			int exit = launch.exitValue();
+			if (exit == 0)
+				SPGLogger.logInfo.accept("Spark job completed successfully ");
+			else if (exit == 1) {
+				SPGLogger.logError.accept("Spark job terminated with errors , please check stdErr.txt and stdOut.txt ");
+				throw new RuntimeException("Spark application failed");
+			}
+		} catch (IOException | InterruptedException e) {
+			SPGLogger.logError.accept("LaunchMainSpark exited with an error " + e.getLocalizedMessage());
+		}
+	}
 
-    public static class Environment {
-        final String name;
-        final String value;
+	/***
+	 * Function to parse spark config into corresponding key value confs.
+	 * 
+	 * @param sparkConfig
+	 *            - unparsed spark configuration as String.
+	 * @return Map of key value pairs.
+	 */
 
-        private Environment(String name, String value) {
-            this.name = name;
-            this.value = value;
-        }
+	public static Map<String, String> buildSparkConf(String conf) {
+		final List<String> parsedConf = Optional.ofNullable(conf).filter(isValidConf)
+				.flatMap(conf1 -> Optional.of(parseConf2.apply(conf1))).orElse(Collections.emptyList());
+		return Environment.toMap(parsedConf.stream().map(cnf -> {
+			int eqIndex = cnf.indexOf("=");
+			String key = cnf.substring(0, eqIndex);
+			String value = cnf.substring(eqIndex + 1);
+			if (key == null) {
+				SPGLogger.logError.accept("Spark conf key cannot be null");
+				throw new IllegalArgumentException("Incorrect spark config values, check the log for more details.");
+			}
+			return Environment.newInstance(key, value);
+		}).collect(toList()));
+	}
 
-        public static Environment newInstance(String name, String value) {
-            return new Environment(name, value);
-        }
+	public static class Environment {
+		final String name;
+		final String value;
 
-        public static Map<String, String> toMap(Environment... env) {
-            return toMap(asList(env));
-        }
+		private Environment(String name, String value) {
+			this.name = name;
+			this.value = value;
+		}
 
-        public static Map<String, String> toMap(List<Environment> env) {
-            return env.stream().filter(en -> en.name != null && en.value != null).collect(Collectors.toMap(e -> e.name, e -> e.value));
-        }
+		public static Environment newInstance(String name, String value) {
+			return new Environment(name, value);
+		}
 
-    }
+		public static Map<String, String> toMap(Environment... env) {
+			return toMap(asList(env));
+		}
+
+		public static Map<String, String> toMap(List<Environment> env) {
+			return env.stream().filter(en -> en.name != null && en.value != null)
+					.collect(Collectors.toMap(e -> e.name, e -> e.value));
+		}
+
+	}
 
 }
--- spark-gradle-runner/src/main/java/com/hari/gradle/spark/plugin/tasks/LaunchSparkTask.java
+++ spark-gradle-runner/src/main/java/com/hari/gradle/spark/plugin/tasks/LaunchSparkTask.java
@@ -1,8 +1,27 @@
 package com.hari.gradle.spark.plugin.tasks;
 
-import com.hari.gradle.spark.plugin.SPGLogger;
-import com.hari.gradle.spark.plugin.Settings;
-import com.hari.gradle.spark.plugin.SparkRunMode;
+import static com.hari.gradle.spark.plugin.Constants.DISTRIBUTED_YARN_CACHE_PATH;
+import static com.hari.gradle.spark.plugin.Constants.HADOOP_FS;
+import static com.hari.gradle.spark.plugin.Constants.HADOOP_HOME;
+import static com.hari.gradle.spark.plugin.Constants.HADOOP_USER_NAME;
+import static com.hari.gradle.spark.plugin.Constants.JOB_DEPS_FILE_SUFFIX;
+import static com.hari.gradle.spark.plugin.Constants.SPARK_CONF_DEPLOY_MODE;
+import static com.hari.gradle.spark.plugin.Constants.SPARK_MAIN_CLASSPATH;
+import static com.hari.gradle.spark.plugin.Constants.SPARK_SCALA_VERSION;
+import static com.hari.gradle.spark.plugin.Constants.STD_ERR;
+import static com.hari.gradle.spark.plugin.Constants.STD_OUT;
+import static com.hari.gradle.spark.plugin.Constants.YARN_CONF_DIR;
+import static com.hari.gradle.spark.plugin.SPGLogger.PROPERTY_SET_VALUE;
+import static com.hari.gradle.spark.plugin.Settings.SETTINGS_EXTN;
+import static com.hari.gradle.spark.plugin.SparkRunMode.getRunMode;
+import static com.hari.gradle.spark.plugin.Utils.getFS;
+import static java.util.Arrays.asList;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.util.List;
+
 import org.apache.hadoop.fs.FileSystem;
 import org.gradle.api.Action;
 import org.gradle.api.DefaultTask;
@@ -10,110 +29,96 @@ import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.process.JavaExecSpec;
 
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.util.List;
-
-import static com.hari.gradle.spark.plugin.Constants.*;
-import static com.hari.gradle.spark.plugin.SPGLogger.PROPERTY_SET_VALUE;
-import static com.hari.gradle.spark.plugin.SparkRunMode.getRunMode;
-import static com.hari.gradle.spark.plugin.Utils.getFS;
-import static java.util.Arrays.asList;
+import com.hari.gradle.spark.plugin.SPGLogger;
+import com.hari.gradle.spark.plugin.Settings;
+import com.hari.gradle.spark.plugin.SparkRunMode;
 
 /**
  * Task to launch spark application via SparkLauncher. Depends on the
  * "CopyDepsTask" task to download all dependencies needed for driver and
  * executor classpath. SparkLauncher is then invoked by launching a separate
  * java process and the main class that would be invoked is
- * {@link com.hari.gradle.spark.plugin.tasks.LaunchMainSpark } }.
- * <p>
+ * {@link com.hari.learning.gradle.spark.plugin.tasks.LaunchMainSpark } }.
+ * 
  * Supported modes are 1) local 2) yarn - cluster (Support for yarn-client is
  * still a subject of discussion)
- *
+ * 
  * @author harim
+ *
  */
 
 public class LaunchSparkTask extends DefaultTask {
 
-    public static final FilenameFilter JAR_FILTER = new FilenameFilter() {
-        @Override
-        public boolean accept(File dir, String name) {
-            return name.endsWith(".jar");
-        }
-    };
+	public static final FilenameFilter JAR_FILTER = new FilenameFilter() {
+		@Override
+		public boolean accept(File dir, String name) {
+			return name.endsWith(".jar");
+		}
+	};
 
-    @TaskAction
-    public void launch() throws IOException {
-        final Project p = getProject();
-        final Settings settings = Settings.getSettings(p);
-        final String mainClass = settings.getMainClass();
-        // mainClass cannot be empty.
-        if (mainClass == null || mainClass.isEmpty())
-            throw new IllegalArgumentException(" Main class cannot be empty");
-        final File libFolder = new File(p.getBuildDir().toPath().toString() + File.separator + "libs");
-        final File[] files = libFolder.listFiles(JAR_FILTER);
-        if (files == null || files.length == 0)
-            throw new IllegalArgumentException("Build failed with generating the output jar.");
-        if (files.length != 1)
-            throw new IllegalArgumentException("More than one version of the generated output jar");
-        String sparkHome = settings.getSparkHome();
-        if (sparkHome == null || sparkHome.isEmpty())
-            throw new IllegalArgumentException("Spark home cannot be empty ");
-        final String classPath = p.getBuildDir().toPath() + File.separator + JOB_DEPS_FILE_SUFFIX + File.separator
-                + "*";
-        SPGLogger.logFine.accept(String.format("Classpath required for invoking SparkLauncher %s ", classPath));
-        // determine whether job needs to run in cluster or local machine.
-        final SparkRunMode runMode = getRunMode.apply(settings.getMaster()).apply(settings.getMode());
-        final File errFile = (settings.getErrRedirect() != null && !settings.getErrRedirect().isEmpty())
-                ? new File(settings.getErrRedirect())
-                : new File(p.getBuildDir().toPath() + File.separator + STD_ERR);
-        SPGLogger.logInfo.accept(String.format("Error redirected to log file %s", errFile.toPath().toString()));
-        final File outFile = (settings.getOutRedirect() != null && !settings.getOutRedirect().isEmpty())
-                ? new File(settings.getOutRedirect())
-                : new File(p.getBuildDir().toPath() + File.separator + STD_OUT);
-        SPGLogger.logInfo.accept(String.format("Output redirected to log file %s", outFile.toPath().toString()));
-        String sparkConf = settings.getSparkConfFilePath();
-        SPGLogger.logInfo.accept(String.format("Spark conf properties file at %s", sparkConf));
-        String dockerImageId = settings.getDockerImageTag();
-        SPGLogger.logInfo.accept(String.format("Docker Image id to be used at Driver %s", dockerImageId));
-
-        final List<Object> prgArgs = asList(new Object[]{settings.getAppName(), settings.getMaster(),
-                files[0].toPath().toString(), mainClass, errFile, outFile, sparkHome, sparkConf, dockerImageId});
-        SPGLogger.logFine.accept("Printing input args to LaunchMainSpark main method");
-        p.javaexec(new Action<JavaExecSpec>() {
-            @Override
-            public void execute(JavaExecSpec spec) {
-                spec.args(prgArgs);
-                spec.setClasspath(
-                        p.fileTree(p.getLayout().getBuildDirectory().dir(JOB_DEPS_FILE_SUFFIX + File.separator)));
-                SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply("SPARK_SCALA", settings.getScalaVersion()));
-                spec.getEnvironment().put(SPARK_SCALA_VERSION, settings.getScalaVersion());
-                SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(HADOOP_HOME, settings.getHadoopHome()));
-                spec.getEnvironment().put(HADOOP_HOME, settings.getHadoopHome());
-                SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(SPARK_MAIN_CLASSPATH, classPath));
-                spec.getEnvironment().put(SPARK_MAIN_CLASSPATH, classPath);
-                if (runMode == SparkRunMode.YARN_CLIENT || runMode == SparkRunMode.YARN_CLUSTER) {
-                    SPGLogger.logFine.accept(
-                            String.format("The spark job is to be submitted in yarn cluster and the deployMode is %s",
-                                    settings.getMode()));
-                    spec.getEnvironment().put(HADOOP_USER_NAME, settings.getHadoopUserName());
-                    SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(HADOOP_USER_NAME, settings.getHadoopUserName()));
-                    spec.getEnvironment().put(YARN_CONF_DIR, settings.getHadoopConf());
-                    SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(YARN_CONF_DIR, settings.getHadoopConf()));
-                    FileSystem fs = getFS.apply(settings.getHadoopHome()).apply(settings.getHadoopConf());
-                    String distYarnCachePath = new StringBuilder(fs.getConf().get(HADOOP_FS))
-                            .append(settings.getJarZipDestPath()).toString();
-                    spec.getEnvironment().put(DISTRIBUTED_YARN_CACHE_PATH, distYarnCachePath);
-                    SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(DISTRIBUTED_YARN_CACHE_PATH, distYarnCachePath));
-                    spec.getEnvironment().put(SPARK_CONF_DEPLOY_MODE, settings.getMode());
-                    SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(SPARK_CONF_DEPLOY_MODE, settings.getMode()));
-                } else if (runMode == SparkRunMode.K8S_CLUSTER) {
-                    SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(SPARK_CONF_DEPLOY_MODE, settings.getMode()));
-                    spec.getEnvironment().put(SPARK_CONF_DEPLOY_MODE, settings.getMode());
-                }
-                spec.setMain(LaunchMainSpark.class.getCanonicalName());
-            }
-        });
-    }
+	@TaskAction
+	public void launch() throws IOException {
+		final Project p = getProject();
+		final Settings settings = (Settings) p.getExtensions().getByName(SETTINGS_EXTN);
+		final String mainClass = settings.getMainClass();
+		// mainClass cannot be empty.
+		if (mainClass == null || mainClass.isEmpty())
+			throw new IllegalArgumentException(" Main class cannot be empty");
+		final File libFolder = new File(p.getBuildDir().toPath().toString() + File.separator + "libs");
+		final File[] files = libFolder.listFiles(JAR_FILTER);
+		if (files == null || files.length == 0)
+			throw new IllegalArgumentException("Build failed with generating the output jar.");
+		if (files.length != 1)
+			throw new IllegalArgumentException("More than one version of the generated output jar");
+		String sparkHome = settings.getSparkHome();
+		if (sparkHome == null || sparkHome.isEmpty())
+			throw new IllegalArgumentException("Spark home cannot be empty ");
+		final String classPath = p.getBuildDir().toPath() + File.separator + JOB_DEPS_FILE_SUFFIX + File.separator
+				+ "*";
+		SPGLogger.logFine.accept(String.format("Classpath required for invoking SparkLauncher %s ", classPath));
+		// determine whether job needs to run in cluster or local machine.
+		final SparkRunMode runMode = getRunMode.apply(settings.getMaster()).apply(settings.getMode());
+		final File errFile = (settings.getErrRedirect() != null && !settings.getErrRedirect().isEmpty())
+				? new File(settings.getErrRedirect())
+				: new File(p.getBuildDir().toPath() + File.separator + STD_ERR);
+		SPGLogger.logInfo.accept(String.format("Error redirected to log file %s", errFile.toPath().toString()));
+		final File outFile = (settings.getOutRedirect() != null && !settings.getOutRedirect().isEmpty())
+				? new File(settings.getOutRedirect())
+				: new File(p.getBuildDir().toPath() + File.separator + STD_OUT);
+		SPGLogger.logInfo.accept(String.format("Output redirected to log file %s", outFile.toPath().toString()));
+		final List<Object> prgArgs = asList(new Object[] { settings.getAppName(), settings.getMaster(),
+				files[0].toPath().toString(), mainClass, errFile, outFile, sparkHome, settings.getSparkConfig() });
+		SPGLogger.logFine.accept("Printing input args to LaunchMainSpark main method");
+		p.javaexec(new Action<JavaExecSpec>() {
+			@Override
+			public void execute(JavaExecSpec spec) {
+				spec.args(prgArgs);
+				spec.setClasspath(
+						p.fileTree(p.getLayout().getBuildDirectory().dir(JOB_DEPS_FILE_SUFFIX + File.separator)));
+				SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply("SPARK_SCALA", settings.getScalaVersion()));
+				spec.getEnvironment().put(SPARK_SCALA_VERSION, settings.getScalaVersion());
+				SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(HADOOP_HOME, settings.getHadoopHome()));
+				spec.getEnvironment().put(HADOOP_HOME, settings.getHadoopHome());
+				SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(SPARK_MAIN_CLASSPATH, classPath));
+				spec.getEnvironment().put(SPARK_MAIN_CLASSPATH, classPath);
+				if (runMode == SparkRunMode.YARN_CLIENT || runMode == SparkRunMode.YARN_CLUSTER) {
+					SPGLogger.logFine.accept(
+							String.format("The spark job is to be submitted in yarn cluster and the deployMode is %s",
+									settings.getMode()));
+					spec.getEnvironment().put(HADOOP_USER_NAME, settings.getHadoopUserName());
+					SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(HADOOP_USER_NAME, settings.getHadoopUserName()));
+					spec.getEnvironment().put(YARN_CONF_DIR, settings.getHadoopConf());
+					SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(YARN_CONF_DIR, settings.getHadoopConf()));
+					FileSystem fs = getFS.apply(settings.getHadoopHome()).apply(settings.getHadoopConf());
+					String distYarnCachePath = new StringBuilder(fs.getConf().get(HADOOP_FS))
+							.append(settings.getJarZipDestPath()).toString();
+					spec.getEnvironment().put(DISTRIBUTED_YARN_CACHE_PATH, distYarnCachePath);
+					SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(DISTRIBUTED_YARN_CACHE_PATH, distYarnCachePath));
+					spec.getEnvironment().put(SPARK_CONF_DEPLOY_MODE, settings.getMode());
+					SPGLogger.logFine.accept(PROPERTY_SET_VALUE.apply(SPARK_CONF_DEPLOY_MODE, settings.getMode()));
+				}
+				spec.setMain(LaunchMainSpark.class.getCanonicalName());
+			}
+		});
+	}
 }
